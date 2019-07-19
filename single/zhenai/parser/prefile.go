8e686a9025f0b3a625037ccc0f195901c77b6fe6
bill.mark <2286449127@qq.com>
2019-07-19 14:50:11 +0800

    用户信息解析器(上)

--- single/zhenai/parser/prefile.go
+++ single/zhenai/parser/prefile.go
@@ -3,19 +3,31 @@ package parser
 import (
 	"regexp"
 	"reptile/single/engine"
+	"reptile/single/model"
 	"strconv"
 )
 
-const ageRe =`<td><span>年龄</span></td>`
+var ageRe =regexp.MustCompile(`<td><span>年龄</span></td>`)
+var marriageRe =regexp.MustCompile(`婚况`)
 
 func ParseProfile(contents []byte)engine.ParseResult{
-	re := regexp.MustCompile(ageRe)
-	match := re.FindSubmatch(contents)
+	profile := model.Profile{}
 
-	if match != nil{
-		age,err := strconv.Atoi(string(match[1]))
-		if err != nil{
+	age,err := strconv.Atoi(extractString(contents,ageRe))  //atoi 将字符串转换为int类型
+	if err != nil{
+		profile.Age = age
+	}
+	profile.Marriage = extractString(contents,marriageRe)
 
-		}
+	result := engine.ParseResult{Items:[]interface{}{profile}}
+	return result
+}
+
+func extractString(contents []byte,re *regexp.Regexp)string{
+	match := marriageRe.FindSubmatch(contents)
+	if len(match)>=2{
+		return string(match[1])
+	}else{
+		return ""
 	}
 }
