310576539fd2046b65f2403deb9f751cf800b5a3
bill.mark <2286449127@qq.com>
2019-07-26 19:19:01 +0800

    用户信息解析_上

--- single/zhenai/parser/prefile.go
+++ single/zhenai/parser/prefile.go
@@ -1,32 +1,39 @@
 package parser
 
 import (
+	"fmt"
+	"github.com/bitly/go-simplejson"
+	"log"
 	"regexp"
 	"reptile/single/engine"
 	"reptile/single/model"
 )
 
-const basic = `basicInfo"/["(*)","(*)","(*)","(*)","(*)","(*)","(*)"\]`
+var re = regexp.MustCompile(`<script>window.__INITIAL_STATE__=(.+);\(function`)
 
 func ParseProfile(contents []byte,name string)engine.ParseResult{
 	profile := model.Profile{}
 	profile.Name = name
 
-	re := regexp.MustCompile(basic)
-	matches := re.FindAllSubmatch(contents,-1)
-
-	for _,m := range matches{
-		//age,err := strconv.Atoi( extractString(m(1) ) )
-		//if err != nil{
-		//	profile.Age = age
-		//}
-
-		profile.Marriage = string(m[0])
-        profile.Xinzuo = string(m[2])
-        profile.Education = string(m[6])
+	match := re.FindSubmatch(contents)
+	if len(match) >=2{
+		json := match[1]
+		fmt.Printf("json:%s\n",json)
 	}
-	result := engine.ParseResult{Items:[]interface{}{profile}}
-	return result
+	return engine.ParseResult{}
+
+	//for _,m := range matches{
+	//	//age,err := strconv.Atoi( extractString(m(1) ) )
+	//	//if err != nil{
+	//	//	profile.Age = age
+	//	//}
+	//	profile.Marriage = string(m[0])
+    //   profile.Xinzuo = string(m[2])
+    //   profile.Education = string(m[6])
+    //   profile.Car = "ben chi 998"
+	//}
+	//result := engine.ParseResult{Items:[]interface{}{profile}}
+	//return result
 }
 
 func extractString(contents []byte)string{
@@ -35,4 +42,12 @@ func extractString(contents []byte)string{
 	}else{
 		return ""
 	}
+}
+
+func parseJson(json []byte)model.Profile{
+	res,err := simplejson.NewJson(json)
+	if err != nil{
+		log.Println("解析json失败")
+	}
+	infos,err := res.Get("objectInfo").Get("basicInfo").Array()
 }
