7d744a943d43ee379287db52c702e2bec14eb020
bill.mark <2286449127@qq.com>
2019-07-29 18:25:23 +0800

    用户信息解析下

--- single/zhenai/parser/prefile.go
+++ single/zhenai/parser/prefile.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/bitly/go-simplejson"
 	"log"
 	"regexp"
@@ -12,36 +11,16 @@ import (
 var re = regexp.MustCompile(`<script>window.__INITIAL_STATE__=(.+);\(function`)
 
 func ParseProfile(contents []byte,name string)engine.ParseResult{
-	profile := model.Profile{}
-	profile.Name = name
-
 	match := re.FindSubmatch(contents)
+	result := engine.ParseResult{}
+
 	if len(match) >=2{
 		json := match[1]
-		fmt.Printf("json:%s\n",json)
+		profile := parseJson(json)
+		profile.Name = name
+		result.Items = append(result.Items,profile)
 	}
-	return engine.ParseResult{}
-
-	//for _,m := range matches{
-	//	//age,err := strconv.Atoi( extractString(m(1) ) )
-	//	//if err != nil{
-	//	//	profile.Age = age
-	//	//}
-	//	profile.Marriage = string(m[0])
-    //   profile.Xinzuo = string(m[2])
-    //   profile.Education = string(m[6])
-    //   profile.Car = "ben chi 998"
-	//}
-	//result := engine.ParseResult{Items:[]interface{}{profile}}
-	//return result
-}
-
-func extractString(contents []byte)string{
-	if len(contents)>=2{
-		return string(contents[1])
-	}else{
-		return ""
-	}
+	return result
 }
 
 func parseJson(json []byte)model.Profile{
@@ -50,4 +29,34 @@ func parseJson(json []byte)model.Profile{
 		log.Println("解析json失败")
 	}
 	infos,err := res.Get("objectInfo").Get("basicInfo").Array()
+	//infos是一个切片,类似是interface
+
+	var profile  model.Profile
+	for k,v := range infos {
+        //fmt.Printf("k:%v,%T\n",k,k)
+		//fmt.Printf("v:%v,%T\n", v, v)
+
+        if e,ok := v.(string);ok{
+        	switch k {
+			case 0:
+				profile.Marriage = e
+			case 1:
+			//年龄:47岁，我们可以设置int类型，所以可以通过另一个json字段来获取
+			   profile.Age = e
+			case 2:
+			  profile.Xingzuo = e
+			case 3:
+			  profile.Height = e
+			case 4:
+			   profile.Weight = e
+			case 6:
+			   profile.Income = e
+			case 7:
+			   profile.Occupation = e
+			case 8:
+			   profile.Education = e
+			}
+		}
+	}
+    return profile
 }
